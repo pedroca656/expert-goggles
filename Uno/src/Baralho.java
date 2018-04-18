fd097b741606be51ee260d7b3f3f5f743b48f25e
pedroca656 <pedro.ivo.fraga@hotmail.com>
2018-04-17 21:52:28 -0300

    Terminar  classe baralho

--- Uno/src/Baralho.java
+++ Uno/src/Baralho.java
@@ -1,32 +1,44 @@
+import java.util.Stack;
 
 public class Baralho {
-	private Carta[] cartas;
+	private Stack<Carta> cartas;
 	
 	public Baralho() {
-		cartas = new Carta[108];
+		cartas = new Stack<Carta>();
 		
 		int valor = 0;
 		char[] cor = new char[2];
+		Carta c;
 		for(int i = 0; i < 4; i++) {
 			if(i == 0) {
-				cor[0] = 'z';
-				cor[1] = 'A';
+				cor[0] = 'A';
+				cor[1] = 'z';
 			}
 			else if(i == 1) {
-				cor[0] = 'm';
-				cor[1] = 'A';
+				cor[0] = 'A';
+				cor[1] = 'm';
 			}
 			else if(i == 2) {
-				cor[0] = 'd';
-				cor[1] = 'V';
+				cor[0] = 'V';
+				cor[1] = 'd';
 			}
 			else if(i == 3) {
-				cor[0] = 'm';
-				cor[1] = 'V';
+				cor[0] = 'V';
+				cor[1] = 'm';
 			}
-			for(int j = 0; j < 14; j++) {
-				
+			
+			c = new Carta('0', cor, valor);
+			
+			for(int j = 1; j < 10; j++) {
+				c = new Carta((char)j, cor, valor);
+				valor++;
+				cartas.push(c);
+				c = new Carta((char)j, cor, valor);
+				valor++;
+				cartas.push(c);
 			}
+			
+			c = new Carta('P', cor, valor);
 		}
 		
 	}
--- Uno/src/Carta.java
+++ Uno/src/Carta.java
@@ -1,21 +1,24 @@
 
 public class Carta {
-	private char[] numeracao;
+	private char numeracao;
 	private char[] cor;
+	private int valor;
 	
-	public Carta(char[] n, char[] c) {
-		numeracao = new char[2];
+	public Carta(char n, char[] c, int v) {
+		//numeracao = new char[2];
 		numeracao = n;
 		
-		cor = new char[2];
+		//cor = new char[2];
 		cor = c;
+		
+		valor = v;
 	}
 
-	public char[] getNumeracao() {
+	public char getNumeracao() {
 		return numeracao;
 	}
 
-	public void setNumeracao(char[] numeracao) {
+	public void setNumeracao(char numeracao) {
 		this.numeracao = numeracao;
 	}
 
@@ -25,6 +28,14 @@ public class Carta {
 
 	public void setCor(char[] cor) {
 		this.cor = cor;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
 	}
 	
 }
